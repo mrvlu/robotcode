4d79b63e15d67aec8a4a0329a33191cf2f0139be
aliumarleaside <ali.umar.work@gmail.com>
2015-04-10 16:20:08 -0400

    Create states.h

---
+++ auton/states.h
@@ -0,0 +1,21 @@
+#ifndef STATES_H
+#define STATES_H
+
+enum moveDir {		//sets direction
+	FORWARDS,
+	RIGHT,
+	BACKWARDS,
+	LEFT
+};
+
+enum armDir {		//direction of arm
+	UP,
+	DOWN
+};
+
+enum clawPos {		//intake state
+	CLOSED,
+	OPEN
+};
+
+#endif
