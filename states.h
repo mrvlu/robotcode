6ae90c80a07541aecf6d94ea0f069da8422cda83
aliumarleaside <ali.umar.work@gmail.com>
2015-04-10 13:35:46 -0400

    Create states.h
    put all states into seperate file for readability

---
+++ states.h
@@ -0,0 +1,21 @@
+#ifndef VARIABLESENUMS_H
+#define VARIABLESENUMS_H
+
+enum moveDir {
+	FORWARDS,
+	RIGHT,
+	BACKWARDS,
+	LEFT
+};
+
+enum armDir {
+	UP,
+	DOWN
+};
+
+enum clawPos {
+	CLOSED,
+	OPEN
+};
+
+#endif
